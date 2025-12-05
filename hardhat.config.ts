840fd60da752456cb948339dd5edb95a86e88ee3
lukaw3d <luka@oasisprotocol.org>
2025-12-05 22:32:53 +0100

    Sapphire networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,37 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {
+  mnemonic: "test test test test test test test test test test test junk",
+  path: "m/44'/60'/0'/0",
+  initialIndex: 0,
+  count: 20,
+  passphrase: "",
+};
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
     settings: {
       evmVersion: "paris",
+    },
+  },
+  networks: {
+    sapphire: {
+      url: "https://sapphire.oasis.io",
+      chainId: 0x5afe,
+      accounts,
+    },
+    "sapphire-testnet": {
+      url: "https://testnet.sapphire.oasis.io",
+      accounts,
+      chainId: 0x5aff,
+    },
+    "sapphire-localnet": {
+      // docker run -it -p8544-8548:8544-8548 ghcr.io/oasisprotocol/sapphire-localnet
+      url: "http://localhost:8545",
+      chainId: 0x5afd,
+      accounts,
     },
   },
 };
