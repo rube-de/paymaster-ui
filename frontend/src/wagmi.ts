bbb41780a4554624abb24340522e42cdeb8fa285
lukaw3d <luka@oasisprotocol.org>
2025-12-11 15:49:34 +0300

    Import sapphire + testnet chain defs

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,47 +1,6 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { defineChain } from 'viem'
-
-export const sapphire = defineChain({
-  id: 0x5afe,
-  name: 'Sapphire',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Sapphire',
-    symbol: 'ROSE',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://sapphire.oasis.io'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Oasis Explorer',
-      url: 'https://explorer.oasis.io/mainnet/sapphire',
-    },
-  },
-});
-
-export const sapphireTestnet = defineChain({
-  id: 0x5aff,
-  name: 'Sapphire Testnet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Sapphire Testnet',
-    symbol: 'TEST',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://testnet.sapphire.oasis.io'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Oasis Explorer',
-      url: 'https://explorer.oasis.io/testnet/sapphire',
-    },
-  },
-});
+import { sapphire, sapphireTestnet } from 'wagmi/chains';
 
 export const sapphireLocalnet = defineChain({
 	id: 0x5afd,
