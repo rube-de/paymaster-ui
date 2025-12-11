10cd8a870872e1ce791873e6a4bcf87b8a0a1ad2
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2025-12-11 12:09:46 +0400

    fix: have a duration instead of an end date, easier to config

--- contracts/interfaces/IRoffle.sol
+++ contracts/interfaces/IRoffle.sol
@@ -102,10 +102,8 @@ interface IRoffle {
     /// @notice Thrown when OPF withdrawal fails
     error WithdrawalFailed();
 
-    /// @notice Thrown when raffle end time is in the past
-    /// @param provided Provided end time
-    /// @param current Current block timestamp
-    error EndTimeInPast(uint256 provided, uint256 current);
+    /// @notice Thrown when duration is zero or invalid
+    error InvalidDuration();
 
     /// @notice Thrown when new end time is after current end time (can only close early)
     error CanOnlyCloseEarly();
--- ignition/modules/Roffle.ts
+++ ignition/modules/Roffle.ts
@@ -5,10 +5,10 @@ const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
 const OPF_CONTRIBUTION = parseEther("100000");
 
 const RoffleModule = buildModule("RoffleModule", (m) => {
-  const endTime = m.getParameter("endTime", ONE_WEEK_IN_SECONDS);
+  const duration = m.getParameter("duration", ONE_WEEK_IN_SECONDS);
   const opfContribution = m.getParameter("opfContribution", OPF_CONTRIBUTION);
 
-  const roffle = m.contract("Roffle", [endTime]);
+  const roffle = m.contract("Roffle", [duration]);
 
   m.call(roffle, "addOPFContribution", [], {
     value: opfContribution,
