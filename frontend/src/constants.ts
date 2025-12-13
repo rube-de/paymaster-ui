620c65448b1d983326bd09fbbdd2e6de99ef4201
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2025-12-13 16:56:01 +0400

    chore: revert package.json and remove the paymaster vault

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,13 +1,8 @@
-import { base } from "viem/chains";
-import { sapphire } from "./wagmi";
+import { sapphire } from 'wagmi/chains'
 
 export const CONTRACTS = {
   roffle: {
     address: "0x" as `0x${string}`,
     chainId: sapphire.id,
-  },
-  paymasterVault: {
-    address: "0x7D3B4dd07bd523E519e0A91afD8e3B325586fb5b" as `0x${string}`,
-    chainId: base.id,
   },
 } as const;
--- frontend/src/hooks/generated/index.ts
+++ frontend/src/hooks/generated/index.ts
@@ -1,2 +1,1 @@
 export * from "./roffle";
-export * from "./paymasterVault";
