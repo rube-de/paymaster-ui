b2a803c685211ab83b1c9ae14bf5221e1761736c
lukaw3d <luka@oasisprotocol.org>
2025-12-16 19:24:39 +0100

    fix: downgraded solidity version to 0.8.24

--- contracts/interfaces/IRoffle.sol
+++ contracts/interfaces/IRoffle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 /// @title IRoffle - Interface for the Oasis Christmas Raffle
 /// @author Oasis Protocol Foundation
